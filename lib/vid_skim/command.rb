1a77c5f9b2013447a28ba0a278e1e70dce3e7b87
Jeff Larson <jlarson@jeff-larsons-imac.local>
2009-09-25 12:26:19 -0400

    command

---
+++ lib/vid_skim/command.rb
@@ -0,0 +1,28 @@
+
+module VidSkim
+  
+  class Command
+    
+    def initialize
+      @command = ARGV.shift
+      @directory = ARGV.shift || '.'
+    end
+    
+    def run
+      case @command
+        when 'install' then run_install
+        when 'build' then run_build
+      end
+    end
+    
+    def run_install
+      
+    end
+    
+    def run_build
+      
+    end
+    
+  end
+  
+end

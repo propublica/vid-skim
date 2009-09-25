f0f5dbe737a9cefb8f6362423acd3b2c6b8509da
Jeremy Ashkenas <jashkenas@gmail.com>
2009-09-25 12:50:31 -0400

    first draft of vidskim installation

--- lib/vid_skim.rb
+++ lib/vid_skim.rb
@@ -5,12 +5,15 @@ require 'rubygems'
 gem 'nokogiri'
 gem 'json'
 
-autoload :YAML, 'yaml'
-autoload :Nokogiri, 'nokogiri'
-autoload :JSON, 'json'
-autoload :ERB, 'erb'
+autoload :YAML,       'yaml'
+autoload :Nokogiri,   'nokogiri'
+autoload :JSON,       'json'
+autoload :ERB,        'erb'
+autoload :FileUtils,  'fileutils'
 
 module VidSkim
   autoload :Transcript "vid_skim/transcript"
   autoload :Tube, "vid_skim/tube"
+  
+  ROOT = File.expand_path(File.dirname(__FILE__) + '/..')
 end
--- lib/vid_skim/command.rb
+++ lib/vid_skim/command.rb
@@ -1,8 +1,10 @@
-
 module VidSkim
-  
+
+  # Command-line `vidskim` client. Handles commands for initial installation
+  # and building out the exported HTML files.
   class Command
     
+    # Creating a VidSkim::Command parses all command-line arguments and options.
     def initialize
       @command = ARGV.shift
       @directory = ARGV.shift || '.'
@@ -15,13 +17,35 @@ module VidSkim
       end
     end
     
+    # Install the example VidSkim folder to a location of your choosing.
     def run_install
-      
+      FileUtils.mkdir_p(@directory) unless File.exists?(@directory)
+      install_dir "#{VidSkim::ROOT}/example/html",   "#{@directory}/html"
+      install_dir "#{VidSkim::ROOT}/example/videos", "#{@directory}/videos"
     end
     
     def run_build
       
     end
+    
+    # Print out `vidskim` usage.
+    def usage
+      
+    end
+    
+    
+    private
+    
+    # Install a file and log the installation. Allow opportunities to back out
+    # of overwriting existing files.
+    def install_dir(source, dest)
+      if File.exists?(dest)
+        print "#{dest} already exists. Overwrite it? (yes/no) "
+        return unless ['y', 'yes', 'ok'].include? gets.chomp.downcase
+      end
+      FileUtils.cp_r(source, dest)
+      puts "installed #{dest}"
+    end
+      
   end
-  
 end
